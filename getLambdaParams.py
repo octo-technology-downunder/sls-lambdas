b02fca8356b93415814329b109ab85ba7c2b3c12
Ilya Trofimov <itrofimov@octo.com>
2018-03-08 18:01:51 +1100

    Fixed call to print() in getLambdaParams.py

--- getLambdaParams.py
+++ getLambdaParams.py
@@ -5,4 +5,4 @@ config_stream = open('serverless.yml', 'r')
 config = yaml.safe_load(config_stream)
 yaml_stream = True
 
-print config['functions'][sys.argv[1]][sys.argv[2]]
+print(config['functions'][sys.argv[1]][sys.argv[2]])
